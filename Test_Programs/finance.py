6e934c91a0bed38d687f28b83c23a47f4774445e
MaximumCodingLife <80234680+MaximumCodingLife@users.noreply.github.com>
2022-03-10 20:49:42 -0700

    Add some testing programs

---
+++ Test_Programs/finance.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+#define the ticker symbol
+tickerSymbol = 'MSFT'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-25')
+
+#see your data
+print(tickerDf)
