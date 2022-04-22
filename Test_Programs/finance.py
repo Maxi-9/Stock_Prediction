a83f9d64646bc7c63d5c377189f568e76f6e203c
MaximumCodingLife <80234680+MaximumCodingLife@users.noreply.github.com>
2022-04-22 13:59:30 -0700

    Fixing the file structure

--- Test_Programs/finance.py
+++ Test_Programs/finance.py
@@ -1,4 +1,7 @@
+from tokenize import String
+from matplotlib import ticker
 import yfinance as yf
+import numpy as np
 #define the ticker symbol
 tickerSymbol = 'MSFT'
 
@@ -6,7 +9,18 @@ tickerSymbol = 'MSFT'
 tickerData = yf.Ticker(tickerSymbol)
 
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-25')
+tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2022-4-14')
 
 #see your data
 print(tickerDf)
+
+closeValues=[]
+dates=[]
+
+    
+closeValues=tickerDf["Close"].values.tolist()
+
+for count in tickerDf.index:
+    dates.append(str(count))
+print(dates)
+
