bbf87a82920b375f21d09dbdf0d1d195472f2038
Maxi-9 <MAX2155621@maricopa.edu>
2024-05-01 22:12:56 -0700

    Created stock parse system to extract stock specific settings

--- stocks.py
+++ stocks.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from enum import Enum
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
 import pandas as pd
@@ -89,49 +90,124 @@ class FeaturesObject:
         self.columns = feature.columns()
 
 
-class Stock_Data:
-    def __init__(self, name: str, period: str, features: [Features]):
-        self.name = name
-        self.period = period
+class StockData:
+    @staticmethod
+    def get_stocks(
+        name: str,
+        features: List[Features],
+        period: str = None,
+        start_date: datetime = None,
+        end_date: datetime = None,
+    ):
+        df = StockData._get_raw_stock(name, period, start_date, end_date)
 
-        # Get Stocks
-        df = self._get_raw_stock()
-
-        # Get TA features
         if (
             Features.RSI in features
             or Features.MACD in features
             or Features.BB in features
         ):
-            df = self._add_features(df)
+            df = StockData._add_features(df)
 
-        # Add shift
         if Features.Prev_Close in features:
-            df = self._add_shift(df)
+            df = StockData._add_shift(df)
 
-        # Add Date
         if Features.Date in features:
-            df = self._add_date(df)
-
-        # Make sure that just the wanted features are present
+            df = StockData._add_date(df)
 
         df = df[Features.to_list(features)]
+        df = StockData._drop_na(df)
+        return df
 
-        df = self._drop_na(df)
-        self.df = df
+    @classmethod
+    def stocks_parse(cls, name: str, features: List[Features]) -> pd.DataFrame:
+        # Split the name into stock and brackets
+        if ":" in name:
+            stock, brackets = name.split(":", maxsplit=1)
+            if "-" in brackets:
+                if "," in brackets:
+                    start_date_str, end_date_str = brackets.split(",")
+                    start_date = datetime.strptime(start_date_str, "%m-%d-%Y")
+                    end_date = datetime.strptime(end_date_str, "%m-%d-%Y")
+                    return cls.get_stocks(
+                        stock, features, start_date=start_date, end_date=end_date
+                    )
+                else:
+                    start_date = datetime.strptime(brackets, "%m-%d-%Y")
+                    return cls.get_stocks(stock, features, start_date=start_date)
+            else:
+                return cls.get_stocks(stock, features, period=brackets)
+        else:
+            return cls.get_stocks(name, features)
 
-    def _get_raw_stock(self) -> pd.DataFrame:
-        ticker = yf.Ticker(
-            self.name,
-        )
+    @staticmethod
+    def _get_raw_stock(
+        name: str,
+        period: str = None,
+        start_date: datetime = None,
+        end_date: datetime = None,
+    ) -> pd.DataFrame:
+        ticker = yf.Ticker(name)
 
-        historical_data = ticker.history(period=self.period)
+        historical_data = ticker.history(start=start_date, end=end_date, period=period)
+
         if historical_data.empty:
-            raise StockNotFound(self.name)
+            raise StockNotFound(name)
 
         historical_data.index = historical_data.index.tz_localize(None).normalize()
-
         return historical_data
+
+    @staticmethod
+    def _add_features(df: pd.DataFrame, window_size: int = 182) -> pd.DataFrame:
+        historical_data = df.copy()
+        features = []
+
+        for i in range(window_size, len(historical_data)):
+            window = historical_data.iloc[i - window_size : i]
+
+            if len(window) < window_size:
+                features.append([float("nan")] * 5)
+            else:
+                rsi = talib.RSI(window["Close"])[-1]
+                macd, _, _ = talib.MACD(
+                    window["Close"], fastperiod=12, slowperiod=26, signalperiod=9
+                )
+                macd_value = macd[-1]
+                bb_upper, bb_middle, bb_lower = talib.BBANDS(
+                    window["Close"], timeperiod=window_size
+                )
+                bb_upper_value, bb_middle_value, bb_lower_value = (
+                    bb_upper[-1],
+                    bb_middle[-1],
+                    bb_lower[-1],
+                )
+                features.append(
+                    [rsi, macd_value, bb_upper_value, bb_middle_value, bb_lower_value]
+                )
+
+        features_df = pd.DataFrame(
+            features,
+            index=historical_data.index[window_size:],
+            columns=["RSI", "MACD", "BB_UPPER", "BB_MIDDLE", "BB_LOWER"],
+        )
+        historical_data = pd.concat([historical_data, features_df], axis=1)
+        return historical_data
+
+    @staticmethod
+    def _add_shift(df: pd.DataFrame) -> pd.DataFrame:
+        df[str(Features.Prev_Close)] = df["Close"].shift(1)
+        return df
+
+    @staticmethod
+    def _add_date(df: pd.DataFrame) -> pd.DataFrame:
+        df["Year"] = df.index.year
+        df["Month"] = df.index.month
+        df["Day"] = df.index.day
+        df["Day_Of_Week"] = df.index.dayofweek  # Monday=0, Sunday=6
+        return df
+
+    @staticmethod
+    def _drop_na(df: pd.DataFrame) -> pd.DataFrame:
+        return df.dropna()
 
     @staticmethod
     def create_rolling_windows(
@@ -174,72 +250,6 @@ class Stock_Data:
         )  # Return the last value of each target window
 
     # Add more features
-    def _add_features(self, df: pd.DataFrame, window_size: int = 182) -> pd.DataFrame:
-        historical_data = df.copy()
-
-        # Initialize an empty list to store the features
-        features = []
-
-        # Iterate over the dataset using a rolling window
-        for i in range(window_size, len(historical_data)):
-            window = historical_data.iloc[i - window_size : i]
-
-            # Check if the window has enough data points
-            if len(window) < window_size:
-                features.append([float("nan")] * 5)  # Append NaN values
-            else:
-                # Calculate the technical indicators for the current window
-                rsi = talib.RSI(window["Close"])[-1]
-                macd, _, _ = talib.MACD(
-                    window["Close"], fastperiod=12, slowperiod=26, signalperiod=9
-                )
-                macd_value = macd[-1]
-                bb_upper, bb_middle, bb_lower = talib.BBANDS(
-                    window["Close"], timeperiod=window_size
-                )
-                bb_upper_value, bb_middle_value, bb_lower_value = (
-                    bb_upper[-1],
-                    bb_middle[-1],
-                    bb_lower[-1],
-                )
-
-                # Append the features to the list
-                features.append(
-                    [rsi, macd_value, bb_upper_value, bb_middle_value, bb_lower_value]
-                )
-
-        # Create a DataFrame from the features list and align it with the original data
-        features_df = pd.DataFrame(
-            features,
-            index=historical_data.index[window_size:],
-            columns=["RSI", "MACD", "BB_UPPER", "BB_MIDDLE", "BB_LOWER"],
-        )
-
-        # Combine the technical indicator features with the original data
-        historical_data = pd.concat([historical_data, features_df], axis=1)
-
-        return historical_data
-
-    def _add_shift(self, df: pd.DataFrame) -> pd.DataFrame:
-        # Shift the Close price to get the previous day's closing price
-        df[str(Features.Prev_Close)] = df["Close"].shift(1)
-
-        return df
-
-    def _add_date(self, df: pd.DataFrame) -> pd.DataFrame:
-        # Assume df is your DataFrame and 'date' is your date column
-
-        # Components of the date
-        df["Year"] = df.index.year
-        df["Month"] = df.index.month
-        df["Day"] = df.index.day
-        df["Day_Of_Week"] = df.index.dayofweek  # Monday=0, Sunday=6
-
-        return df
-
-    def _drop_na(self, df: pd.DataFrame) -> pd.DataFrame:
-        # Drop any rows with missing values
-        return df.dropna()
 
     # Train:Test split, higher ratio means more training data
     @staticmethod
@@ -262,14 +272,6 @@ class Stock_Data:
 
         target = df[prediction.columns()]
         return features, target
-
-    def getTodayStocks(self, stock: str):
-        ticker = yf.Ticker(stock)
-
-        # Get the latest information (current day)
-        latest_data = ticker.history(period="1d")
-
-        return latest_data
 
 
 """
